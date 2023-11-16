1d38c3639ef63e730893eddeadd720e03e6cb8b6
Diwansu <diwansu531@gmail.com>
2023-11-16 12:41:15 +0530

    Project Completed

--- game.js
+++ game.js
@@ -1,5 +1,88 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+ const box1 = document.getElementById("number1")
+ let number1 = Math.round (Math.random()*100);
+ 
 
+ const box2= document.getElementById("number2")
+ let number2= Math.round(Math.random()*100);
+  
+ box1.innerText= number1 
+ box2.innerText = number2 
 // Iteration 3: Make the options button functional
+ const greaterthan = document.getElementById("greater-than") 
+ const equalto = document.getElementById("equal-to")
+ const lesserthan = document.getElementById("lesser-than")
+var score = 0;
+ greaterthan.onclick = suj ;
+ function suj(){
+    if (number1>number2){
+        score++ 
+        reset (timerId)}else{
+            localStorage.setItem("score",score);
+            location.href ='./gameover.html'
+        }
+        number1 = Math.round (Math.random()*100);
+        number2 = Math.round (Math.random()*100);
+
+        box1.innerText= number1 
+        box2.innerText= number2 
+    }
+  
+    equalto.onclick = function(){
+        if(number1===number2){
+            score++
+            reset (timerId)
+        }else{
+            localStorage.setItem("score",score);
+            location.href='./gameover.html'
+        }
+        number1 = Math.round (Math.random()*100);
+        number2 = Math.round (Math.random()*100);
+
+        box1.innerText= number1 
+        box2.innerText= number2  
+       
+    }
+
+    lesserthan.onclick = function() {
+        if(number1<number2){
+            score++ 
+            reset (timerId)
+        }else{
+            localStorage.setItem("score",score);
+            location.href = './gameover.html'
+        }
+        number1 = Math.round (Math.random()*100);
+        number2 = Math.round (Math.random()*100);
+
+        box1.innerText= number1 
+        box2.innerText= number2 
+    }
 
 // Iteration 4: Build a timer for the game
+  var time=5 ;
+  var timer=document.getElementById("timer") 
+  var timerId ; 
+
+   function showTimer (){
+    time=5 
+    timer.innerText=time 
+    timerId =setInterval(()=>{
+      time-- ;
+
+    if(time==0){
+        localStorage.setItem("score",score);
+        location.href='./gameover.html';
+    }
+    timer.innerText=time ;
+    },1000)
+    }
+
+    function reset (timerId){
+        clearInterval(timerId) 
+        showTimer()
+    }
+
+    showTimer() 
+
+
--- score.js
+++ score.js
@@ -1,1 +1,16 @@
 // Iteration 5: Store the player score and display it on the game over screen
+  
+let points = localStorage.getItem("score") 
+
+let scoreBoard = document.getElementById("score-board") 
+scoreBoard.innerText = points ;
+console.log(points) ;
+
+let playAgain = document.getElementById("play-again-button") 
+ playAgain.onclick = prev 
+ function prev(){
+    location.href = './index.html'
+ }  
+
+
+
